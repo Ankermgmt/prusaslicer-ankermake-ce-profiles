7e7623c51a557cee61d14be4524755bee0b35742
just-trey <trey.wentz@gmail.com>
2023-05-12 00:48:24 -0400

    fix powershell script

--- install.ps1
+++ install.ps1
@@ -1,36 +1,66 @@
 # Look for PrusaSlicer config directory
-$prusaSlicerDir = Join-Path -Path $env:APPDATA -ChildPath 'PrusaSlicer'
+$selection
+$availablePrusaSlicerDirs = @()
 
-# Check if PrusaSlicer config directory exists
-if (!(Test-Path -Path $prusaSlicerDir -PathType Container)) {
+$prusaSlicerDirs = @(
+    $HOME + '\AppData\Roaming\PrusaSlicer'
+    $HOME + '\AppData\Roaming\PrusaSlicer-alpha'
+    $HOME + '\AppData\Roaming\PrusaSlicer-beta'
+)
+
+foreach ($dir in $prusaSlicerDirs) {
+    if (Test-Path -Path $dir) {
+        $availablePrusaSlicerDirs += $dir
+    }
+}
+
+# Check if any PrusaSlicer config directory exists
+if ($availablePrusaSlicerDirs.Length -eq 0) {
     Write-Host "PrusaSlicer config directory not found."
     exit 1
 }
 
-# Set the destination directory
-$prusaVendorDir = Join-Path -Path $prusaSlicerDir -ChildPath 'vendor'
+If ($availablePrusaSlicerDirs.Length -gt 1) {
+    Write-Host "Multiple PrusaSlicer directories found."
+    Write-Host "Please choose which one you want to install the profiles to:"
 
-# Open in windows explorer $prusaSlicerDir
-Invoke-Item -Path "$prusaVendorDir"
-
-# Check if destination directory or files exist
-$folders = @('AnkerMakeCE')
-$files = @('AnkerMakeCE.idx', 'AnkerMakeCE.ini')
-
-# Copy all folders from ./vendor to $prusaSlicerDir/vendor
-foreach ($folder in $folders) {
-    $srcDir = Join-Path -Path $PSScriptRoot/vendor -ChildPath $folder
-    $destDir = Join-Path -Path $prusaVendorDir -ChildPath $folder
-
-    Write-Host "Copying $srcDir to $destDir/$folder"
-    Copy-Item -Path $srcDir -Destination $destDir/$folder -Recurse -Force
+    # Build the choices menu
+    $choices = @()
+    For ($index = 0; $index -lt $availablePrusaSlicerDirs.Count; $index++) {
+        Write-Host "[$index] $($availablePrusaSlicerDirs[$index])"
+        # $choices += New-Object System.Management.Automation.Host.ChoiceDescription [$index], ($availablePrusaSlicerDirs[$index])
+    }
+    $chosen = Read-host [Your selection]
+    $selection = $availablePrusaSlicerDirs[$chosen]
 }
+else { $selection = $availablePrusaSlicerDirs[0] }
 
-# Copy all files from ./vendor to $prusaSlicerDir/vendor
-foreach ($file in $files) {
-    $srcFile = Join-Path -Path $PSScriptRoot/vendor -ChildPath $file
-    $destFile = Join-Path -Path $prusaVendorDir -ChildPath $file
+Write-Host $selection
 
-    Write-Host "Copying $srcFile to $destFile"
-    Copy-Item -Path $srcFile -Destination $destFile -Force
+$title = "Installing Profiles to $selection" 
+$question = 'Are you sure you want to proceed?'
+
+$choices = New-Object Collections.ObjectModel.Collection[Management.Automation.Host.ChoiceDescription]
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&Yes'))
+$choices.Add((New-Object Management.Automation.Host.ChoiceDescription -ArgumentList '&No'))
+
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+ 
+    # Copy all folders from ./vendor to $prusaSlicerDir
+    $srcDir = Join-Path -Path $PSScriptRoot -ChildPath 'vendor'
+ 
+    # $destDir = Join-Path -Path $selection -ChildPath 'vendor'
+    $destDir = $selection
+ 
+    Write-Host "Copying $srcDir to $destDir"
+    Copy-Item -Path $srcDir -Destination $destDir -Recurse -Force
+    Write-Host 'Installation completed'
+    
+    # Set the destination directory
+    $prusaVendorDir = Join-Path -Path $destDir -ChildPath 'vendor'
+    Invoke-Item -Path "$prusaVendorDir"
+}
+else {
+    Write-Host 'Installation cancelled'
 }
